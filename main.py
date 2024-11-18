8412784b7a605223ace1eead2de74d3b1c15b02b
Alex Gulyan <97192407+alexgulyan16@users.noreply.github.com>
2024-11-17 20:13:20 -0700

    Microservice files

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def send_email():
         return {"Error": "Check your email credentials and try again."}, 401
    
 if __name__ == '__main__': 
-   app.run(host="127.0.0.1", port=5000, debug=True)
+   app.run(host="127.0.0.1", port=8000, debug=True)
